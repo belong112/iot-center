69e6609b51a302a7d12394871fb0f6e03c2e4402
Belong112 <belongbelong223@gmail.com>
2019-08-10 21:35:18 +0800

    Updates

---
+++ precache-manifest.684b0055e7314a776d7bf3ee2fbbd1e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b07d32cb9dedb0aef855632ca80b6cb",
+    "url": "/iot-center/index.html"
+  },
+  {
+    "revision": "092f19a98df8f3c7352b",
+    "url": "/iot-center/static/css/main.ccf4d003.chunk.css"
+  },
+  {
+    "revision": "5b3ff7f4299c8438c2bf",
+    "url": "/iot-center/static/js/2.fdc223bb.chunk.js"
+  },
+  {
+    "revision": "092f19a98df8f3c7352b",
+    "url": "/iot-center/static/js/main.e185a087.chunk.js"
+  },
+  {
+    "revision": "116f6f8a21cd32beebc4",
+    "url": "/iot-center/static/js/runtime~main.2f683def.js"
+  },
+  {
+    "revision": "8d530f571c2c48ca38ed0b9a72658589",
+    "url": "/iot-center/static/media/4.8d530f57.jpg"
+  },
+  {
+    "revision": "9910c84c400307feb4d0ae07c83fa3dd",
+    "url": "/iot-center/static/media/6.9910c84c.jpg"
+  },
+  {
+    "revision": "212e8f96cea7a8823a39601273c81743",
+    "url": "/iot-center/static/media/home2.212e8f96.png"
+  },
+  {
+    "revision": "a66b7dd57d918824b9b5abe7a8457e40",
+    "url": "/iot-center/static/media/horse.a66b7dd5.jpg"
+  },
+  {
+    "revision": "c181f79c911aaba87515c8228ca0de6d",
+    "url": "/iot-center/static/media/morty.c181f79c.jpg"
+  },
+  {
+    "revision": "e6f14e3f4947c17330448165aeb4c830",
+    "url": "/iot-center/static/media/ntu.e6f14e3f.png"
+  },
+  {
+    "revision": "d768b1a62c522f9c6df02beb0129a2af",
+    "url": "/iot-center/static/media/rick.d768b1a6.jpg"
+  }
+]);
